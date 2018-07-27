0cc1792d4e666ae38cf7d391bc5b934771afc452
ashankv <ashank.verma@gmail.com>
2018-07-26 20:24:38 -0700

    Initial commit

---
+++ CloudNotes/DetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewController.swift
+//  CloudNotes
+//
+//  Created by Ashank Verma on 7/20/18.
+//  Copyright © 2018 Ashank Verma. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+        
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.title = "";
+
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CloudNotes/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  CloudNotes
+//
+//  Created by Ashank Verma on 7/20/18.
+//  Copyright © 2018 Ashank Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var notes = [Any]()
+    
+    @IBAction func addButton(_ sender: Any) {
+    
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+
+        editButtonItem.tintColor = UIColor(red:1.00, green:0.76, blue:0.03, alpha:1.0)
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target:self, action: #selector(insertNewObject(_:)))
+        addButton.tintColor = UIColor(red:1.00, green:0.76, blue:0.03, alpha:1.0)
+        navigationItem.rightBarButtonItem = addButton
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc
+    func insertNewObject(_ sender: Any) {
+        notes.insert(NSDate(), at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                
+                let note = notes[indexPath.row] as! NSDate
+                let controller = (segue.destination) as! DetailViewController
+                
+                controller.detailItem = note
+                //controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                //controller.navigationItem.leftItemsSupplementBackButton = true
+        
+            }
+        }
+    }
+    
+    // Table View
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let note = notes[indexPath.row] as! NSDate
+        cell.textLabel!.text = note.description
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            notes.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
+    
+}
+
